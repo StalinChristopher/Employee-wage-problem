500798066bfbff8167f326eacc360cfd265658c9
Stalin CD <stalincd@120AMBBLR-Samyad-R-Jain.local>
2021-08-21 23:04:55 +0530

    welcome message in the master branch

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -3,8 +3,8 @@ package MainPackage;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to the Employe wage computation program");
+		
 	}
 
 }
