c88aa579e356a6706dd53d4fbbc843508011fe6f
Stalin CD <stalincd@120AMBBLR-Samyad-R-Jain.local>
2021-08-21 23:15:10 +0530

    Added UC1 logic to find if employee is present or not in a new branch UC1

--- src/MainPackage/Employee.java
+++ src/MainPackage/Employee.java
@@ -1,6 +1,12 @@
 package MainPackage;
 
 public class Employee {
-
+	boolean isPresent() {
+		double emp_val=Math.floor(Math.random()*10)%2;
+		if(emp_val==1)
+			return true;
+		else
+			return false;
+	}
 
 }
--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -3,8 +3,14 @@ package MainPackage;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Employe wage computation program");
-		
+		System.out.println("Welcome to the Employee wage computation program");
+		Employee em1=new Employee();
+		//UC1
+		if(em1.isPresent())
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+			
 	}
 
 }
