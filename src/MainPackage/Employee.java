6ae8c066b3ca74cfb439af24f2a9a866e459f3df
Stalin CD <stalincd@120AMBBLR-Samyad-R-Jain.local>
2021-08-21 23:36:56 +0530

    Added method for calculating daily wage in UC2 branch

--- src/MainPackage/Employee.java
+++ src/MainPackage/Employee.java
@@ -1,12 +1,23 @@
 package MainPackage;
 
 public class Employee {
+	final double regularFullDayWorkingHours=8;
+	
+	double wagePerHour;
+	Employee(int wagePerHour){
+		this.wagePerHour=wagePerHour;
+	}
 	boolean isPresent() {
 		double emp_val=Math.floor(Math.random()*10)%2;
 		if(emp_val==1)
 			return true;
 		else
 			return false;
+	}
+	
+	double calculatedailyWage() {
+		return regularFullDayWorkingHours*wagePerHour;
+		
 	}
 
 }
--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee wage computation program");
-		Employee em1=new Employee();
+		Employee em1=new Employee(20);
 		//UC1
 		if(em1.isPresent())
 			System.out.println("Employee is present");
 		else
 			System.out.println("Employee is absent");
-			
+		
+		//UC2
+		System.out.println("Daily wage of the employee is "+ em1.calculatedailyWage());
 	}
 
 }
