310b60c5e95f6b3ebdc15af8d388e4aa870d51c8
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-08-31 18:26:22 +0530

    Refactored code and made changes to logic

--- src/MainPackage/Employee.java
+++ src/MainPackage/Employee.java
@@ -1,53 +1,78 @@
 package MainPackage;
 
 public class Employee {
-	private final double regularFullDayWorkingHours=8;
-	private final double partTimeFullDayWorkingHours=4;
+	//private final double regularFullDayWorkingHours=8;
+	//private final double partTimeFullDayWorkingHours=4;
 	private final double monthlyWorkingDays=20;
 	private final int FULL_TIME = 1;
 	private final int PART_TIME = 2;
-	
-	double wagePerHour;
-	int emp_type;
-	double dailyWage;
-	Employee(int wagePerHour,int emp_type){
-		this.wagePerHour=wagePerHour;
-		this.emp_type=emp_type;
+	private final double wagePerHour = 20;
+	private int empWorkingHours;
+	private int emp_type;
+	private double dailyWage;
+	private int total_Days_worked = 0;
+	private int total_hours_worked = 0;
+
+	public void isPresent() {
+		double emp_val=Math.floor(Math.random()*10)%3;
+		if(emp_val == 1) {
+			emp_type = FULL_TIME;
+			empWorkingHours = 8;
+			System.out.println("Full time employee is present");
+		}else if(emp_val == 2 ) {
+			emp_type = PART_TIME;
+			empWorkingHours = 4;
+			System.out.println("Part time employee is present");
+		}else {
+			empWorkingHours = 0;
+			System.out.println("Employee is absent");
+		}
+
 	}
-	boolean isPresent() {
-		double emp_val=Math.floor(Math.random()*10)%2;
-		if(emp_val==1)
-			return true;
-		else
-			return false;
-	}
 	
-	double calculatedailyWage() {
-		return regularFullDayWorkingHours*wagePerHour;
+	public double calculatedailyWage() {
+		return empWorkingHours*wagePerHour;
 		
 	}
 	
 	
-	double calculatePartTimeDailyWage() {
-		return partTimeFullDayWorkingHours*wagePerHour;
+	public double calculatePartTimeDailyWage() {
+		return empWorkingHours*wagePerHour;
 	}
 	
-	double calculateDailyWageSwitch() {
+	public double calculateDailyWageSwitch() {
 		switch(emp_type) {
-		case FULL_TIME: dailyWage=regularFullDayWorkingHours*wagePerHour;
+		case FULL_TIME: dailyWage=empWorkingHours*wagePerHour;
 				break;
-		case PART_TIME: dailyWage=partTimeFullDayWorkingHours*wagePerHour;
+		case PART_TIME: dailyWage=empWorkingHours*wagePerHour;
 				break;
 		}
 		return dailyWage;
 	}
 	
-	double calculateMonthlyWage() {
+	public double calculateMonthlyWage() {
 		return monthlyWorkingDays*calculateDailyWageSwitch();
 	}
 	
-	double calculateWagePerHour(int hours) {
+	public double calculateWagePerHour(int hours) {
 		return hours*wagePerHour;
 	}
+	
+	public void daysAndHoursConditionWage() {
+		if(!(emp_type==0)) {
+			while(total_Days_worked<20 && total_hours_worked<100) {
+				int randomHours = (int)Math.floor(Math.random()*((8-4)+1)+4);
+				total_Days_worked++;
+				System.out.println("\nDay no: "+total_Days_worked);
+				System.out.println("Hours worked today : "+randomHours);
+				total_hours_worked+=randomHours;
+			}
+			System.out.println("\nTotal hours worked: "+total_hours_worked);
+			System.out.println("Total days worked: "+total_Days_worked);
+			System.out.println("Total wages earned by employee: "+this.calculateWagePerHour(total_hours_worked));
+		}
+		
+	}
+	
 
 }
--- src/MainPackage/EmployeeMain.java
+++ src/MainPackage/EmployeeMain.java
@@ -4,35 +4,19 @@ public class EmployeeMain {
 
 	public static void main(String[] args) {
 		//setting values for reg and part time employees to be used during obj creation
-		final int REG_EMP=1;
-		final int PART_TIME=2; 
 		int total_hours_worked=0;
 		int total_Days_worked=0;
 		System.out.println("Welcome to the Employee wage computation program");
 		
 		//Creating a regular employee who has an wage per hour of 20
-		Employee em1=new Employee(20,REG_EMP);
+		Employee em1=new Employee();
 		
-		if(em1.isPresent())
-			System.out.println("Employee em1 is present");
-		else
-			System.out.println("Employee em1 is absent");
+		em1.isPresent();
 		
-		System.out.println("Daily wages of employee em1 of type regular is "+em1.calculateDailyWageSwitch());
-		System.out.println("Monthly wage of employee em1 of type regular is "+em1.calculateMonthlyWage());
+		System.out.println("Daily wages of employee "+em1.calculateDailyWageSwitch());
+		System.out.println("Monthly wage of employee "+em1.calculateMonthlyWage());
 		
-		
-		
-		while(total_Days_worked<20 && total_hours_worked<100) {
-			int randomHours = (int)Math.floor(Math.random()*((8-4)+1)+4);
-			System.out.println("Hours worked today : "+randomHours);
-			total_Days_worked++;
-			System.out.println("Day no: "+total_Days_worked);
-			total_hours_worked+=randomHours;
-		}
-		System.out.println("Total hours worked: "+total_hours_worked);
-		System.out.println("Total days worked: "+total_Days_worked);
-		System.out.println("Total wages earned by em1 : "+em1.calculateWagePerHour(total_hours_worked));
+		em1.daysAndHoursConditionWage();
 	}
 
 }
