b361d4c181e8a84c9d4202bbb11b6b9aba3804af
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-05 19:03:52 +0530

    Added logic to compute wages for multiple companies

--- src/MainPackage/EmployeeMain.java
+++ src/MainPackage/EmployeeMain.java
@@ -11,16 +11,14 @@ public class EmployeeMain {
 		//To randomly select the employee type and assign working hours based on type of employee
 		em1.isPresent();
 		
+		/*
 		System.out.println("Daily wages of employee "+em1.calculateDailyWageSwitch());
 		System.out.println("Monthly wage of employee "+em1.calculateMonthlyWage());
+		*/
 		
-		//Company A
-		System.out.println("\nCompany A\n");
-		em1.daysAndHoursConditionWage(20,25,80);
-		
-		//Company B
-		System.out.println("\nCompany B\n");
-		em1.daysAndHoursConditionWage(30,26,90);
-	}
+		em1.computeWage(20, 15, 40);
+				
 
+	}
 }
+
