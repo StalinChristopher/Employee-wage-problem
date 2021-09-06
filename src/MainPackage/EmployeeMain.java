4a37108d85abdc799e03a6a8c9696e87e74b34d4
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-06 16:21:32 +0530

    minor changes to UC11 changed object of main method to interface object and initialized it with Employee class constructor

--- src/MainPackage/EmployeeMain.java
+++ src/MainPackage/EmployeeMain.java
@@ -5,13 +5,12 @@ public class EmployeeMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Employee wage computation program");
 		
-		//Creating a employee object
-		Employee employee=new Employee();
+		ComputeEmpWage builder = new Employee();
 		
 
-		employee.addCompanyEmpWage("Dmart", 60, 20, 80);
-		employee.addCompanyEmpWage("Infosys", 200, 18, 85);
-		employee.computeWage();
+		builder.addCompanyEmpWage("Dmart", 60, 20, 80);
+		builder.addCompanyEmpWage("Infosys", 200, 18, 85);
+		builder.computeWage();
 				
 
 	}
